4164a79d2ef727934d2c8970b62045c2acc4ccc5
KuanChih Wang <n789142536@hotmail.com>
2019-03-23 14:20:06 +0800

    add times

--- guess-num.py
+++ guess-num.py
@@ -1,16 +1,20 @@
 import random
 num = random.randint(1, 100)
-
+times = 0
 print('猜數字, 請猜一個介於1~100(包含)之間的數字')
 
 while True:
 	gus = input('請輸入數字: ')
 	gus = int(gus)
+	times = times + 1
 
 	if gus == num:
+		print('猜測次數: ', times, '次')
 		print('恭喜猜對了！')
 		break
 	elif gus > num:
+		print('猜測次數: ', times, '次')
 		print('猜錯了，答案還要再小一點！再猜一次吧！')
 	else:
+		print('猜測次數: ', times, '次')
 		print('猜錯了，數字還要再大一點！再猜一次吧！')
